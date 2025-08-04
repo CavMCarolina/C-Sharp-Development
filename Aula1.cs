993d7fa3d5c5e4993138430eac1040dc72cf6161
Carolina <143201623+CavMCarolina@users.noreply.github.com>
2025-08-04 11:51:06 -0300

    Teste

---
+++ Aula1.cs
@@ -0,0 +1,27 @@
+﻿using System; //biblioteca do sistema
+
+// Separa os blocos
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Magenta;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("teste teste");
+
+            // Escreve sem enter
+            Console.Write("aaaaaaaaaaaaaaaaaaaaaaaaaaa");
+
+            Console.Write("adadasdadsa");
+
+            // Sonzinho
+            Console.Beep(000, 1);
+        }
+    }
+}
